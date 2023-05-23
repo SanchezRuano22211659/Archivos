16eeb7a10a578a3b31bacf1a8b2865720c60d9cd
Alexis Sánchez <126355162+SanchezRuano22211659@users.noreply.github.com>
2023-05-23 19:55:48 +0000

    added binary writer

--- archivosProgram/Program.cs
+++ archivosProgram/Program.cs
@@ -41,24 +41,52 @@ class ProductsDB
             string[] column = row.Split();
             products.Add(new Product(column[0],column[1],Convert.ToDecimal(column[2])));
         }
+        textIn.Close();
 
         return products;
     }
+    public static void SaveProductsBin(List<Product> products)
+    {
+        FileStream fs = new FileStream("productsdata.bin",FileMode.Create,FileAccess.Write);
+        BinaryWriter binOut = new BinaryWriter(fs);
+        foreach(var p in products)
+        {
+            binOut.Write(p.Code + "|");
+            binOut.Write(p.Description + "|");
+            binOut.Write(p.Price);
+        }
+        binOut.Close();
+    }
+    public static List<Product> GetProductsBin()
+    {
+        FileStream fs = new FileStream("productsdata.bin",FileMode.Open,FileAccess.Read);
+        BinaryReader binIn = new BinaryReader(fs);
+        while(binIn.PeekChar() != -1)
+        {
+            string Code = binIn.ReadString();
+            string Description = binIn.ReadString();
+            decimal Price = binIn.ReadDecimal();
+            Product product = new Product(Code,Description,Price);
+        }
+        binIn.Close();
+        return GetProducts();
+    }
 }
+   
 
  class Program
 {
     private static void Main(string[] args)
     {
-        // List<Product> products = new List<Product>();
-        // products.Add(new Product ("1200", "a", 1324234m));
-        // products.Add(new Product ("200", "b", 221345m));
-        // products.Add(new Product ("1234", "c", 3123123m));
-        // products.Add(new Product ("13554", "d", 312m));
-        // products.Add(new Product ("5832", "e", 3123m));
+         List<Product> products = new List<Product>();
+         products.Add(new Product ("1200", "a", 1324234m));
+         products.Add(new Product ("200", "b", 221345m));
+         products.Add(new Product ("1234", "c", 3123123m));
+         products.Add(new Product ("13554", "d", 312m));
+         products.Add(new Product ("5832", "e", 3123m));
 
 
-        // ProductsDB.SaveProducts(products);
-       List<Product> products = ProductsDB.GetProducts();
+        ProductsDB.SaveProductsBin(products);
+       // List<Product> products = ProductsDB.GetProducts();
     }
 }
